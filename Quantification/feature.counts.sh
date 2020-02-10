5189cad95b0a432e9e0d4b83b6873943b7b6ae86
jperera-bel <jperera@imim.es>
2020-02-10 16:36:25 +0100

    readme mkdir; featurecounts space bug

--- Quantification/feature.counts.sh
+++ Quantification/feature.counts.sh
@@ -22,29 +22,29 @@ OUTDIR=$2
 ANNOTGENE=/bicoh/MARGenomics/Analysis_Files/Annot_files_GTF
 
 featureCounts -T $SLURM_CPUS_PER_TASK -p -t exon --largestOverlap -g gene_name -a $ANNOTGENE/gencode.v29.primary_assembly.annotation.gtf -o $OUTDIR/CountsTable.txt \
-$BAMDIR/0101-1_S1.bam\
-$BAMDIR/0102-2_S2.bam\
-$BAMDIR/0201-3_S3.bam\
-$BAMDIR/0202-4_S4.bam\
-$BAMDIR/0401-rep-11_S4.bam\
-$BAMDIR/0402-rep-12_S5.bam\
-$BAMDIR/0601-solar_S12.bam\
-$BAMDIR/0602-solar_S13.bam\
-$BAMDIR/0701-9_S5.bam\
-$BAMDIR/0702-15_S6.bam\
-$BAMDIR/0801-rep-21_S9.bam\
-$BAMDIR/0802-rep-22_S10.bam\
-$BAMDIR/0901-16_S2.bam\
-$BAMDIR/0902-17_S3.bam\
-$BAMDIR/1001-23_S8.bam\
-$BAMDIR/1002-rep-24_S9.bam\
-$BAMDIR/1201-27_S6.bam\
-$BAMDIR/1202-28_S7.bam\
-$BAMDIR/HC0101-rep-18_S13.bam\
-$BAMDIR/HC01201_S11.bam\
-$BAMDIR/HC01301_S12.bam\
-$BAMDIR/HC0401-rep-20_S8.bam\
-$BAMDIR/HC0501-10_S7.bam\
-$BAMDIR/HC0601-8_S1.bam\
-$BAMDIR/HC0801-rep-30_S10.bam\
+$BAMDIR/0101-1_S1.bam \
+$BAMDIR/0102-2_S2.bam \
+$BAMDIR/0201-3_S3.bam \
+$BAMDIR/0202-4_S4.bam \
+$BAMDIR/0401-rep-11_S4.bam \
+$BAMDIR/0402-rep-12_S5.bam \
+$BAMDIR/0601-solar_S12.bam \
+$BAMDIR/0602-solar_S13.bam \
+$BAMDIR/0701-9_S5.bam \
+$BAMDIR/0702-15_S6.bam \
+$BAMDIR/0801-rep-21_S9.bam \
+$BAMDIR/0802-rep-22_S10.bam \
+$BAMDIR/0901-16_S2.bam \
+$BAMDIR/0902-17_S3.bam \
+$BAMDIR/1001-23_S8.bam \
+$BAMDIR/1002-rep-24_S9.bam \
+$BAMDIR/1201-27_S6.bam \
+$BAMDIR/1202-28_S7.bam \
+$BAMDIR/HC0101-rep-18_S13.bam \
+$BAMDIR/HC01201_S11.bam \
+$BAMDIR/HC01301_S12.bam \
+$BAMDIR/HC0401-rep-20_S8.bam \
+$BAMDIR/HC0501-10_S7.bam \
+$BAMDIR/HC0601-8_S1.bam \
+$BAMDIR/HC0801-rep-30_S10.bam \
 $BAMDIR/HC0901-rep-31_S11.bam
