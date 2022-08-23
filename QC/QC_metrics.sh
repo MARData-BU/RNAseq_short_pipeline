f447867f7dfb1bc73ad0bc8107c9150a64f84f57
Ariadna A. Terrades <63337883+ariadnaaterrades@users.noreply.github.com>
2022-08-23 09:55:37 +0200

    Update QC_metrics.sh
    Job arrays included

--- QC/QC_metrics.sh
+++ QC/QC_metrics.sh
@@ -3,19 +3,19 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu 6Gb     # Memory in MB
 #SBATCH -J QC_metrics          # job name
-#SBATCH -o QC_metrics.%j.out    # File to which standard out will be written
-#SBATCH -e QC_metrics.%j.err    # File to which standard err will be written
+#SBATCH -o logs/QC_metrics.%j.out    # File to which standard out will be written
+#SBATCH -e logs/QC_metrics.%j.err    # File to which standard err will be written
 
 # QC Analysis of RNASeq samples of project: 
 
-PROJECT=20190826_AGimenez_FIS_UFred
-
+PROJECT=$1
+BATCH=$2
 # Prepare variables
 #------------------
 
 path=/bicoh/MARGenomics
 DIR=${path}/${PROJECT}
-QC=$DIR/QC
+QC=${DIR}/QC/${BATCH}
 
 # Load modules
 #------------------
@@ -24,7 +24,7 @@ module load R/4.0.0
 
 # Prepare folders
 #------------------
-mkdir $DIR/QC/multiQC
+mkdir -p $QC/multiQC
 
 # Move to QC folder
 cd $QC
@@ -39,8 +39,8 @@ multiqc . -f -o $QC/multiQC
 #=================================#
 # Create table 4 QC presentation  #
 #=================================#
-LANES=4
+LANES=1
 R=2 # paired end
-COLOR=FALSE #color for duplications
+COLOR=TRUE #color for duplications
 
-Rscript $DIR/QC/table4QCpresentation.R $QC $LANES $R $COLOR # QC dir, numer of lanes, paired end, color for duplications
+Rscript $DIR/QC/PhaseI/smallRNASeq/table4QCpresentation.R $QC $LANES $R $COLOR # QC dir, numer of lanes, paired end, color for duplications
